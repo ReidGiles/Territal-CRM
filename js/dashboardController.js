f647da01fc2a5a7121f9dc22be95e7f13014b910
Reid Giles <33459088+ReidGiles@users.noreply.github.com>
2020-05-15 10:36:56 +0100

    Filter implemented

--- js/dashboardController.js
+++ js/dashboardController.js
@@ -3,33 +3,94 @@ angular.module('TerritalCRM', [])
         $scope.properties = [];
         $scope.tenants = [];
         $scope.propertyTenantLink = [];
-        $scope.filter = [];
-        $scope.test = "Angular test";
-        
-        $http.get("/Territal-CRM/ajax/getProperties.php").then(function (data, status, headers, config) {
-            angular.forEach(data.data, function (value, key) {
-                $scope.properties.push(value);
-            });
-            $http.get("/Territal-CRM/ajax/getTennants.php").then(function (data, status, headers, config) {
-                angular.forEach(data.data, function (value, key) {
-                    $scope.tenants.push(value);               
-                });
+        $scope.unoccupiedProperties = [];
+        $scope.showOccupied = true;
+        $scope.showUnoccupied = false;
+        $scope.propertySwitch;
+        $scope.filter = [];       
+        $scope.rentFilter;
 
-                angular.forEach($scope.properties, function (value, key) {
-                    var property = value;
-                    angular.forEach($scope.tenants, function (value, key) {
-                        var tenant = value;
-                        if (property.TenantID === tenant.TenantID){
-                            $scope.propertyTenantLink.push({key: property, value: tenant});
-                        }
+        var populate = function(){
+            $http.get("/Territal-CRM/ajax/getProperties.php").then(function (data, status, headers, config) {
+                angular.forEach(data.data, function (value, key) {
+                    $scope.properties.push(value);
+                    if (!value.TenantID){
+                        $scope.unoccupiedProperties.push(value);
+                    }
+                });
+                $http.get("/Territal-CRM/ajax/getTennants.php").then(function (data, status, headers, config) {
+                    angular.forEach(data.data, function (value, key) {
+                        $scope.tenants.push(value);               
+                    });
+    
+                    angular.forEach($scope.properties, function (value, key) {
+                        var property = value;
+                        angular.forEach($scope.tenants, function (value, key) {
+                            var tenant = value;
+                            if (property.TenantID === tenant.TenantID){
+                                $scope.propertyTenantLink.push({key: property, value: tenant});
+                            }
+                        });
                     });
                 });
             });
-        });
+        }
+
+        var depopulate = function(){
+            $scope.properties = [];
+            $scope.tenants = [];
+            $scope.propertyTenantLink = [];
+            $scope.unoccupiedProperties = [];
+        }
+
+        populate();
 
         $scope.getTenant = function($tenantID){
             $http.get("/Territal-CRM/ajax/getTennant.php?tenantID" + $tenantID).then(function (data, status, headers, config) {              
                 var test = data;
+            });
+        }
+
+        $scope.toggleProperties = function(){            
+            if ($scope.propertySwitch === "occupied"){
+                $scope.showOccupied = true;
+                $scope.showUnoccupied = false;
+            }
+            else{
+                $scope.showOccupied = false;
+                $scope.showUnoccupied = true;
+            }
+        }
+
+        $scope.filterRent = function(){
+            depopulate();
+            $http.get("/Territal-CRM/ajax/getProperties.php").then(function (data, status, headers, config) {
+                angular.forEach(data.data, function (value, key) {
+                    $scope.properties.push(value);
+                    if (!value.TenantID){
+                        $scope.unoccupiedProperties.push(value);
+                    }
+                });
+                $http.get("/Territal-CRM/ajax/getTennants.php").then(function (data, status, headers, config) {
+                    angular.forEach(data.data, function (value, key) {
+                        $scope.tenants.push(value);               
+                    });
+    
+                    angular.forEach($scope.properties, function (value, key) {
+                        var property = value;
+                        angular.forEach($scope.tenants, function (value, key) {
+                            var tenant = value;
+                            if ($scope.rentFilter !== "nolimit"){
+                                if ((property.TenantID === tenant.TenantID) && (property.PropertyRent < parseInt($scope.rentFilter)) && property.PropertyRent > (parseInt($scope.rentFilter)) - 250){
+                                    $scope.propertyTenantLink.push({key: property, value: tenant});
+                                }
+                            }
+                            else if ((property.TenantID === tenant.TenantID)){
+                                $scope.propertyTenantLink.push({key: property, value: tenant});
+                            }
+                        });
+                    });
+                });
             });
         }
     });
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -10,6 +10,10 @@
             }
             //Include users class
             require_once('classes/users.classes.php');
+            require_once('classes/properties.classes.php');
+            require_once('classes/tenants.classes.php');
+            $propertyObj = new properties($DBH);
+            $tenantObj = new tenants($DBH);
             $userObj = new users($DBH); //Lets pass through our DB connection
             $userProfile = $userObj->getUser($_SESSION['userData']['UserID']); //Call the getUser function
             // If the user has set their forename then greet them, else encourage them to update their profile.
@@ -24,61 +28,91 @@
         ?>
     </div>
 
-    <div id="propertyCard" class="card container mt-5">
-        <h1 class="card-header">Properties</h1>
-        <div class="card-body dashboardCardData">       
-            <table class="table table-hover">
-                <thead>
-                <tr>{{test}}
-                    <th>Address</th>
-                    <th>Name</th>
-                    <th>Monthly Rent</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                require_once('classes/properties.classes.php');
-                require_once('classes/tenants.classes.php');
-                $propertyObj = new properties($DBH);
-                $tenantObj = new tenants($DBH);
-                $properties = $propertyObj->getProperties($_SESSION['userData']['UserID']);
-                foreach ($properties as &$property) {
-                    $tenant = $tenantObj->getTenant($_SESSION['userData']['UserID'], $property['TenantID']);
-                    echo "<tr>";
-                    echo "<td><a href='index.php?p=editProperty&propertyID=" . $property['PropertyID'] . "'" . '>' . $property['PropertyAddress'] . "</a></td>";
-                    
-                    if ($tenant){
-                        echo "<td><a href='index.php?p=editProperty&propertyID=" . $property['PropertyID'] . "'" . '>' . $tenant['TenantForename'] . " " . $tenant['TenantSurname'] . "</a></td>";
-                    }
-                    else echo "<td><a href='index.php?p=editProperty&propertyID=" . $property['PropertyID'] . "'" . '>' . "No tenant on record</a></td>";
-                    echo "<td><a href='index.php?p=editProperty&propertyID=" . $property['PropertyID'] . "'" . '>' . "£" . $property['PropertyRent'] . "</a></td>";
-                    echo "</tr>";
-                }
-                ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-    <div id="clientCard" class="card container mt-5">
-        <h1 class="card-header">Tenants</h1>
+    <div ng-show="showOccupied" id="clientCard" class="card container mt-5">
+        <h1 id="occupiedHeader" class="card-header">Occupied Properties</h1>
         <div class="card-body dashboardCardData">       
             <table class="table table-hover">
                 <thead>
                 <tr>
-                    <th>Name</th>
                     <th>Address</th>
+                    <th>Tenant</th>
                     <th>Monthly Rent</th>
                 </tr>
                 </thead>
                 <tbody>
                     <tr ng-repeat="property in propertyTenantLink">
-                        <td>{{property.value.TenantForename + " " + property.value.TenantSurname}}</td>
-                        <td>{{property.key.PropertyAddress}}</td>
-                        <td>{{property.key.PropertyRent}}</td>
+                        <td><a href='index.php?p=editProperty&propertyID={{property.key.PropertyID}}'>{{property.key.PropertyAddress}}</a></td>
+                        <td><a href='index.php?p=editProperty&propertyID={{property.key.PropertyID}}'>{{property.value.TenantForename + " " + property.value.TenantSurname}}</a></td>                       
+                        <td><a href='index.php?p=editProperty&propertyID={{property.key.PropertyID}}'>{{property.key.PropertyRent}}</a></td>
                     </tr>
                 </tbody>
             </table>
+        </div>
+    </div>
+
+    <div ng-show="showUnoccupied" id="clientCard" class="card container mt-5">
+        <h1 class="card-header">Inactive Properties</h1>
+        <div class="card-body dashboardCardData">       
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>Address</th>
+                    <th>Tenant</th>
+                    <th>Monthly Rent</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr ng-repeat="property in unoccupiedProperties">
+                        <td><a href='index.php?p=editProperty&propertyID={{property.PropertyID}}'>{{property.PropertyAddress}}</a></td>
+                        <td><a href='index.php?p=editProperty&propertyID={{property.PropertyID}}'>Currently Unoccupied</a></td>                       
+                        <td><a href='index.php?p=editProperty&propertyID={{property.PropertyID}}'>{{property.PropertyRent}}</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div id="filterCard" class="card container mt-5">
+        <h1 class="card-header">Filter</h1>
+        <div class="form-group">
+            <label for="OccupationFilter">Occupation:</label>
+            <select ng-model="propertySwitch" ng-change="toggleProperties()" class="form-control" id="OccupationFilter" name="OccupationFilter">
+                <option value="occupied">Occupied</option>
+                <option value="unoccupied">Unoccupied</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="rentFilter">Monthly Rent:</label>
+            <select ng-model="rentFilter" ng-change="filterRent()" class="form-control" id="rentFilter" name="rentFilter">
+                <option value="nolimit">No limit</option>
+                <option value="500">Under 500</option>
+                <option value="750">500 to 749</option>
+                <option value="1000">750 to 999</option>
+                <option value="1250">1000 to 1249</option>
+                <option value="1500">1250 to 1499</option>
+                <option value="2000">1500 to 1999</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="tenantFilter">Tenant:</label>
+            <select class="form-control" id="tenantFilter" name="tenantFilter">
+            <option>All</option>
+            <?php
+                $tenants = $tenantObj->getTenants($_SESSION['userData']['UserID']);
+                foreach ($tenants as &$tenant) {
+                    $tenantID = $tenant['TenantID'];
+                    echo '<option value="' . $tenantID . '">' . $tenant['TenantForename'] . ' ' . $tenant['TenantSurname'] . '</option>';
+                }
+            ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="rentFilter">Address:</label>
+            <select class="form-control" id="rentFilter" name="rentFilter">
+                <option>All</option>
+                <option>0 - 499,999</option>
+                <option>500,000 - 999,999</option>
+            </select>
         </div>
     </div>
 </div>
